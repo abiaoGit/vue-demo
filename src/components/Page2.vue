502445811128948a899ba5fea53341a2b179e471
peibiao <peibiao@didichuxing.com>
2018-02-27 17:03:17 +0800

    update code

---
+++ src/components/Page2.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Page2',
+  data () {
+    return {
+      msg: 'Welcome to page2'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
